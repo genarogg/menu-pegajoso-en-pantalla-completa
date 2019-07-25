3420b26603a7a74b06d1a33a5c2e5981a4b44035
Genaro Gonzalez <genarrogg@gmail.com>
2019-07-24 21:17:32 -0400

    mostrar()

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ var menu = document.getElementById('contenedorMenu2');
 var cerrar = document.getElementById('cerrar');
 var item = document.getElementById('item');
 
+function mostrar(){
+    menu.style.padding = "26px 26px 200vw 200vw";
+    cerrar.style.display = "block";
+    item.style.display = "flex";
+
+}
